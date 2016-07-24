b2625a6a5b4f280b28918966c9c726e1bb4757fb
Daniel Häfele <haefele@xemio.net>
2016-07-24 21:08:17 +0200

    Improve how the nuget package is generated
    It now automatically uses all files in the release output directory.
    They also have the correct directory structure now.
    All dependencies are read from the project.json file.
    Fix encoding issue with the nuspec file.

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,16 +1,43 @@
 Function Pack($root, $assemblyName)
 {
+	#Get Assembly Path
     $assemblyPath = "$root\src\$assemblyName\bin\Release\$assemblyName.dll";
 
-	$version = [System.Reflection.Assembly]::LoadFile($assemblyPath).GetName().Version;
-	$versionAsString = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build);
+    #Get project.json
+	$projectJsonPath = "$root\src\$assemblyName\project.json";
+	$projectJson = Get-Content $projectJsonPath -Raw | ConvertFrom-Json;
 
+	#Get nuspec
 	$nuspecPath = $root + "\nuget\" + $assemblyName + ".nuspec";				
+	$nuspec = [xml](Get-Content $nuspecPath); 
 
-	[xml]$content = Get-Content $nuspecPath; 
-	$content.package.metadata.version = $versionAsString;
-	$content.Save($nuspecPath);
+	#Update nuspec
 
+    #Update version
+	$version = [System.Reflection.Assembly]::LoadFile($assemblyPath).GetName().Version;
+	$nuspec.package.metadata.version = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build);
+	
+    #Update dependencies
+    $dependency = $nuspec.package.metadata.dependencies.dependency;
+
+    Write-Host $dependency;
+
+	foreach($element in $projectJson.dependencies.psobject.Properties) 
+	{
+        $currentDependency = $dependency.Clone();
+        
+        $currentDependency.id = $element.name;
+        $currentDependency.version = $element.value;
+
+        $nuspec.package.metadata.dependencies.AppendChild($currentDependency);
+	}
+
+    $nuspec.package.metadata.dependencies.RemoveChild($dependency);
+
+	#Save nuspec
+	$nuspec.Save($nuspecPath);
+
+	#Create package
 	& nuget pack $nuspecPath;
 }
 
