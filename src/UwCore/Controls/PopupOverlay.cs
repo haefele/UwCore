ed925fc08dae4e2350ced145ef1d4c89b7a850bd
Daniel Häfele <haefele@xemio.net>
2016-07-28 13:02:10 +0200

    Start basic work on a popup overlay

---
+++ src/UwCore/Controls/PopupOverlay.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UwCore.Controls
+{
+    public class PopupOverlay : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register(
+            nameof(IsOpen), typeof(bool), typeof(PopupOverlay), new PropertyMetadata(default(bool)));
+
+        public bool IsOpen
+        {
+            get { return (bool) GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        public PopupOverlay()
+        {
+            this.DefaultStyleKey = typeof(PopupOverlay);
+        }
+    }
+}
