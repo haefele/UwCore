4b149e027514e8abe5ba359ee114ae24491b3f46
Daniel Häfele <haefele@xemio.net>
2016-09-05 12:36:48 +0200

    Close the popup overlay if the user clicks or taps outside of it
    This fixes issue #1.

---
+++ src/UwCore/Themes/PopupOverlayStyles.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Caliburn.Micro;
+using UwCore.Services.Navigation;
+using INavigationService = UwCore.Services.Navigation.INavigationService;
+
+namespace UwCore.Themes
+{
+    public partial class PopupOverlayStyles : ResourceDictionary
+    {
+        public PopupOverlayStyles()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void PopupOverlayBackground_OnPointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            var popupNavigationService = IoC.Get<INavigationService>().Popup as PopupNavigationService;
+            popupNavigationService?.Close();
+        }
+    }
+}
---
+++ src/UwCore/Themes/generic.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Caliburn.Micro;
+using UwCore.Controls;
+using UwCore.Services.Navigation;
+using INavigationService = UwCore.Services.Navigation.INavigationService;
+
+namespace UwCore.Themes
+{
+    public partial class generic : ResourceDictionary
+    {
+        public generic()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
