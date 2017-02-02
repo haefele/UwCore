c9ce98133207d791476938e39935400e0abb9559
Daniel Häfele <haefele@xemio.net>
2017-02-02 14:23:49 +0100

    Add a separator line to the secondary-actions in the hamburger menu

---
+++ src/UwCore/Converter/IntegerToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace UwCore.Converter
+{
+    public class IntegerToVisibilityConverter : IValueConverter
+    {
+        public bool Inverse { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int == false)
+                return DependencyProperty.UnsetValue;
+
+            int i = (int)value;
+
+            return i > 0 && this.Inverse == false
+                ? Visibility.Visible 
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
