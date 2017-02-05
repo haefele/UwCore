21e151caf2fb3876cd70787103c2299ae627caf4
Daniel Häfele <haefele@xemio.net>
2017-02-05 16:22:21 +0100

    Fix loading overlay above a popup

--- src/UwCoreTest/Views/MahPopup/MahPopupViewModel.cs
+++ src/UwCoreTest/Views/MahPopup/MahPopupViewModel.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Reactive;
+using System.Threading.Tasks;
 using UwCore;
 
 namespace UwCoreTest.Views.MahPopup
 {
     public class MahPopupViewModel : UwCoreScreen
     {
+        public UwCoreCommand<Unit> Loading { get; }
+
+        public MahPopupViewModel()
+        {
+            this.Loading = UwCoreCommand
+                .Create(_ => Task.Delay(TimeSpan.FromSeconds(2)))
+                .ShowLoadingOverlay("asdflöjkasdfölkj asdf");
+        }
     }
 }
